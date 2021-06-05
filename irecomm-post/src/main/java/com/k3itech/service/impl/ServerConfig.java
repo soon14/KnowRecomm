ff975ac3e099bec80e4bf6907f294ffec7572be3
wengxf <wengowl@gmail.com>
2021-06-05 17:07:58 +0800

    加入手动触发计算匹配接口

---
+++ irecomm-post/src/main/java/com/k3itech/service/impl/ServerConfig.java
@@ -0,0 +1,35 @@
+package com.k3itech.service.impl;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author:dell
+ * @since: 2021-06-05
+ */
+@Component
+public class ServerConfig  implements ApplicationListener<WebServerInitializedEvent> {
+    private int serverPort;
+
+    public String getUrl() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "http://" + address.getHostAddress() + ":" + this.serverPort;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.serverPort = event.getWebServer().getPort();
+    }
+}
+
+
+
